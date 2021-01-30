e424f8fa4ac4ca6fecbd7174619e78214dd6158b
Matt Coston <mattcoston7@gmail.com>
2021-01-30 13:32:58 -0600

    modifed password field

--- music-dashboard/src/App.js
+++ music-dashboard/src/App.js
@@ -1,17 +1,17 @@
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./App.css";
 import ButtonAppBar from "./components/buttonappbar";
-import UsernameField from './components/usernamefield';
+import logIn from "./components/usernamefield";
+import UsernameField from "./components/usernamefield";
 function App() {
   return (
     <div className="App">
-      
       <ButtonAppBar />
       <UsernameField />
     </div>
--- music-dashboard/src/components/usernamefield.js
+++ music-dashboard/src/components/usernamefield.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   textField: {
     marginLeft: theme.spacing(50),
     marginRight: theme.spacing(50),
-    width: '25ch',
-    marginTop: theme.spacing(1)
+    width: "25ch",
+    marginTop: theme.spacing(1),
   },
 }));
 
@@ -21,7 +21,6 @@ export default function UsernameField() {
   return (
     <div className={classes.root}>
       <div>
-        
         <TextField
           label="Username"
           id="margin-dense"
@@ -30,6 +29,7 @@ export default function UsernameField() {
           margin=""
         />
         <TextField
+          type="password"
           label="Password"
           id="margin-dense"
           className={classes.textField}
@@ -37,7 +37,6 @@ export default function UsernameField() {
           margin=""
         />
       </div>
-      
     </div>
   );
 }
