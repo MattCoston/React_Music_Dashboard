4b1871eaa828eaf22fe43c4090e9c33d5cf027e8
Matt Coston <mattcoston7@gmail.com>
2021-02-02 13:40:25 -0600

    changed switch

--- music-dashboard/src/components/switches.js
+++ music-dashboard/src/components/switches.js
@@ -1,85 +1,24 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import { purple } from "@material-ui/core/colors";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Switch from "@material-ui/core/Switch";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const IOSSwitch = withStyles((theme) => ({
-  root: {
-    width: 42,
-    height: 26,
-    padding: 0,
-    margin: theme.spacing(1),
-  },
-  switchBase: {
-    padding: 1,
-    "&$checked": {
-      transform: "translateX(16px)",
-      color: theme.palette.common.white,
-      "& + $track": {
-        backgroundColor: "#52d869",
-        opacity: 1,
-        border: "none",
-      },
-    },
-    "&$focusVisible $thumb": {
-      color: "#52d869",
-      border: "6px solid #fff",
-    },
-  },
-  thumb: {
-    width: 24,
-    height: 24,
-  },
-  track: {
-    borderRadius: 26 / 2,
-    border: `1px solid ${theme.palette.grey[400]}`,
-    backgroundColor: theme.palette.grey[50],
-    opacity: 1,
-    transition: theme.transitions.create(["background-color", "border"]),
-  },
-  checked: {},
-  focusVisible: {},
-}))(({ classes, ...props }) => {
-  return (
-    <Switch
-      focusVisibleClassName={classes.focusVisible}
-      disableRipple
-      classes={{
-        root: classes.root,
-        switchBase: classes.switchBase,
-        thumb: classes.thumb,
-        track: classes.track,
-        checked: classes.checked,
-      }}
-      {...props}
-    />
-  );
-});
-function Switches() {
-  const [state, setState] = React.useState({
-    online: false,
-  });
+function SwitchesSize() {
+  const [checked, setChecked] = React.useState(false);
+  const [message, setMessage] = React.useState(false)
 
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
+  const toggleChecked = () => {
+    setChecked((prev) => !prev);
+    setMessage((prev) => !prev)
   };
 
   return (
-    <FormControlLabel
-      control={
-        <IOSSwitch
-          checked={state.online}
-          onChange={handleChange}
-          name="online"
-        />
-      }
-      label="Turn on Dashboard"
-    />
+    <FormGroup>
+      <FormControlLabel
+        control={<Switch checked={checked} onChange={toggleChecked} message={console.log("test text")}/>}
+        label="Dashboard Control"
+      />
+    </FormGroup>
   );
 }
-
-export default Switches;
+export default SwitchesSize;
